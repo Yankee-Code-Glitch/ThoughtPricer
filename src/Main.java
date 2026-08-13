51d6fb4c2ab8abdbb5746fada1910f1953c7462f
hahde <hahdejus@gmail.com>
2026-08-13 13:40:44 -0600

    Hotfix for initializing messages ResourceBundle in Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,12 @@
 import java.util.ResourceBundle;
+import java.util.Locale;
 
 public class Main {
     public static ResourceBundle messages;
 
     public static void main(String[] args) {
+        messages = ResourceBundle.getBundle("messages", Locale.US);
+
         ThoughtPricer.calculateSecondsPerCurrency();
     }
 }
